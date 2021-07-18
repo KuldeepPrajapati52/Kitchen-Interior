0bffeeebe86f210488e3e97117b820b3f6a28071
Kuldeep Prajapati <prajapatikuldeep52@gmail.com>
2021-07-18 21:35:11 +0530

    color

--- src/myComponents/headerComponent/headerStyle.js
+++ src/myComponents/headerComponent/headerStyle.js
@@ -21,7 +21,7 @@ export const useStyles = makeStyles((theme) => ({
     alignItems: "center",
   },
   navbar: {
-    backgroundColor: Theme.colors.primary1,
+    backgroundColor: Theme.colors.navbarColor,
     color: Theme.colors.navbarTextColor,
 
   },
